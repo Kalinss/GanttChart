bb8d3220eaac251316dbc3c49de5f26f3956c7bf
Kalinss <Kalinss16@gmail.com>
2020-09-15 14:31:16 +0400

    Updates

--- precache-manifest.80c7fa95abe6775582a26951853c0c4b.js
+++ precache-manifest.80c7fa95abe6775582a26951853c0c4b.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "53d20d68fcd1688dc86d5a1df91d9f51",
+    "revision": "41cd1318d3afe5d9bb0bc507f7fc1095",
     "url": "/gantt-chart/index.html"
   },
   {
-    "revision": "bb6bcae7f5fda76bc67b",
-    "url": "/gantt-chart/static/css/main.3e6480b5.chunk.css"
+    "revision": "cf61dbda131b4931b8be",
+    "url": "/gantt-chart/static/css/main.684008eb.chunk.css"
   },
   {
     "revision": "3b105cba291a9e0ef5f3",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/gantt-chart/static/js/2.94d85eed.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bb6bcae7f5fda76bc67b",
+    "revision": "cf61dbda131b4931b8be",
     "url": "/gantt-chart/static/js/main.9cf2d8dc.chunk.js"
   },
   {
